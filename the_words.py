ed0d0d6e7dd922f8fec74cf8c591c447b4be4c14
molybrine <tonipassos54@gmail.com>
2026-01-23 19:55:00 +0000

    add ditransitive builder and test set generation

--- the_words.py
+++ the_words.py
@@ -114,3 +114,102 @@ class BigGenerator:
         r['v3'] = f'{sn} {vv}'
         r['v4'] = f'{vv} {sn}'
         return r
+
+    def _build_d(self, s, v, io, do, t='present'):
+        vv = self._do_tense(v, t)
+        sn, doa = self._case(s, do)
+        return dict(
+            v1=f'{s} {vv} {io} {do}',
+            v2=f'{vv} {s} {io} {do}',
+            v3=f'{sn} {vv} {io} {doa}',
+            v4=f'{vv} {sn} {io} {doa}',
+        )
+
+    def generate_test_set(self, nt=20, ni=20, nd=20, na=10):
+        _init_words()
+        ss = []
+        nn = list(WORDS['n'].keys())
+        vv = list(WORDS['v'].keys())
+        tt = ['past', 'present', 'future']
+
+        for i in range(nt):
+            _s = self.rng.choice(nn)
+            _v = self.rng.choice(vv)
+            _o = self.rng.choice([x for x in nn if x != _s])
+            _t = tt[i % 3]
+            _vars = self._build_t(f'la {_s}', _v, f'la {_o}', _t)
+            _es = WORDS['n'][_s]
+            _ev = self._eng_v(WORDS['v'][_v], _t)
+            _eo = WORDS['n'][_o]
+            ss.append(S(
+                english=f'the {_es} {_ev} the {_eo}',
+                verb_type='transitive', has_adjective=False,
+                has_pronoun=False, tense=_t,
+                word_count=len(_vars['v1'].split()),
+                **_vars,
+            ))
+
+        _iv = {'mangxas': 'eats', 'trinkas': 'drinks', 'legas': 'reads', 'skribas': 'writes'}
+        _seen = set()
+        _i = 0
+        while _i < ni:
+            _s = self.rng.choice(nn)
+            _v = self.rng.choice(list(_iv.keys()))
+            _t = tt[_i % 3]
+            _vars = self._build_i(f'la {_s}', _v, _t)
+            if _vars['v1'] in _seen:
+                continue
+            _seen.add(_vars['v1'])
+            _es = WORDS['n'][_s]
+            _ev = self._eng_v(_iv[_v], _t)
+            ss.append(S(
+                english=f'the {_es} {_ev}',
+                verb_type='intransitive', has_adjective=False,
+                has_pronoun=False, tense=_t,
+                word_count=len(_vars['v1'].split()),
+                **_vars,
+            ))
+            _i += 1
+
+        _dv = ['donas', 'portas', 'metas']
+        for i in range(nd):
+            _s = self.rng.choice(nn)
+            _v = self.rng.choice(_dv)
+            _io = self.rng.choice([x for x in nn if x != _s])
+            _do = self.rng.choice([x for x in nn if x not in (_s, _io)])
+            _t = tt[i % 3]
+            _vars = self._build_d(f'la {_s}', _v, f'al la {_io}', f'la {_do}', _t)
+            _es = WORDS['n'][_s]
+            _ev = self._eng_v(WORDS['v'][_v], _t)
+            _eio = WORDS['n'][_io]
+            _edo = WORDS['n'][_do]
+            ss.append(S(
+                english=f'the {_es} {_ev} to the {_eio} the {_edo}',
+                verb_type='ditransitive', has_adjective=False,
+                has_pronoun=False, tense=_t,
+                word_count=len(_vars['v1'].split()),
+                **_vars,
+            ))
+
+        _aa = list(WORDS['a'].keys())
+        for i in range(na):
+            _s = self.rng.choice(nn)
+            _adj = self.rng.choice(_aa)
+            _v = self.rng.choice(vv)
+            _o = self.rng.choice([x for x in nn if x != _s])
+            _t = tt[i % 3]
+            _vars = self._build_t(f'la {_adj} {_s}', _v, f'la {_o}', _t)
+            _es = WORDS['n'][_s]
+            _ea = WORDS['a'][_adj]
+            _ev = self._eng_v(WORDS['v'][_v], _t)
+            _eo = WORDS['n'][_o]
+            ss.append(S(
+                english=f'the {_ea} {_es} {_ev} the {_eo}',
+                verb_type='transitive', has_adjective=True,
+                has_pronoun=False, tense=_t,
+                word_count=len(_vars['v1'].split()),
+                **_vars,
+            ))
+
+        return ss
+
